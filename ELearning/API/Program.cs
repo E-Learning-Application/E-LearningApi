e468a4d3e86fed86257fdd6610ce1661676cf0ca
Naru <omarnaru2002@gmail.com>
2025-07-14 13:59:36 +0300

    applied EF Core migrations at startup

--- ELearning/API/Program.cs
+++ ELearning/API/Program.cs
@@ -133,11 +133,11 @@ namespace API
             });
             var app = builder.Build();
 
-            //using (var scope = app.Services.CreateScope())
-            //{
-            //    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-            //    dbContext.Database.Migrate();
-            //}
+            using (var scope = app.Services.CreateScope())
+            {
+                var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+                dbContext.Database.Migrate();
+            }
 
 
             app.MapOpenApi();
